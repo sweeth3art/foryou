caea52fe0faa02fbe1f986374e9e53dc0d362394
sweeth3art <121766092+sweeth3art@users.noreply.github.com>
2023-01-02 01:17:27 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const menuBar = document.querySelector(".menu-bar");
+const menuNav = document.querySelector(".menu");
+
+menuBar.addEventListener('click', () => {
+  menuNav.classList.toggle("menu-active");
+});
+
+const navBar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  const windowPosition = window.scrollY > 0 ;
+  navBar.classList.toggle("scrolling-active", windowPosition)
+});
+let play = document.getElementById("play");
+function playMusic() {
+  let audio = new Audio("audip.mp3");
+  audio.play()
+}
+play.addEventListener("click", playMusic);
